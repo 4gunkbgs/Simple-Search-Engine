38a01ca044daf2441460504f58b8b8d214da44b1
A409FA <satriaagung1111@gmail.com>
2022-07-02 23:05:07 +0800

    fixing pagination & search key

--- app/Http/Controllers/BrowseController.php
+++ app/Http/Controllers/BrowseController.php
@@ -21,7 +21,7 @@ class BrowseController extends Controller
 
         // fungsi untuk mengubah spasi menjadi %20 
         // agar bisa dimasukkan ke query
-        function convertSearchKey($key)
+        function ubahSearchKey($key)
         {
             $key = (trim($key));
             $searchKey = '';
@@ -67,11 +67,9 @@ class BrowseController extends Controller
         // menggabungkan input user dengan url pada apache solr
         // $url = "http://localhost:8983/solr/mal_core/select?indent=true&q.op=OR&q=title_txt_en%3A%20" . convertSearchKey($request->search);
 
-        // http://localhost:8983/solr/mal_core/select?indent=true&q.op=OR&q=title_txt_en%3Aone%2Bpiece%0Abody_txt_en%3Aone%2Bpiece
-        // http://localhost:8983/solr/mal_core/select?indent=true&q.op=OR&q=title_txt_en%3Aone%2Bpiece%0Abody_txt_en%3Aone%2Bpiece
-
-        $url = "http://localhost:8983/solr/mal_core/select?indent=true&q.op=OR&q=title_txt_en%3A" . convertSearchKey($request->search) . "%0Abody_txt_en%3A" . convertSearchKey($request->search);
-        // supaya data yang tampil bisa lebih dari default = 10
+        //query yang digunakan 
+        $url = "http://localhost:8983/solr/mal_core/select?indent=true&q.op=OR&q=title_txt_en%3A" . ubahSearchKey($request->search) . "%0Abody_txt_en%3A" . ubahSearchKey($request->search) . "%0Agenre_txt_en%3A" . ubahSearchKey($request->search);
+        // supaya data yang tampil bisa lebih dari default
         $url = $url . "&rows=50";
 
         // get kontennya
@@ -87,9 +85,12 @@ class BrowseController extends Controller
         $perPage = 5;
         // cek jika jumlah ditemukan pada solr > 5
         if ($fixed_result['response']['numFound'] > 5) {
-
+            $setResult = $fixed_result['response']['numFound'];
+            if ($setResult > 50) {
+                $setResult = 50;
+            }
             // hitung banyak halaman yang akan ditampilkan dari data yang ditemukan
-            $banyakHalaman = penghitungPaginate($fixed_result['response']['numFound'], $perPage);
+            $banyakHalaman = penghitungPaginate($setResult, $perPage);
             // paginate halaman tersebut
             $hasil = paginate($hasil, $perPage, $request->halaman);
         }
@@ -97,7 +98,7 @@ class BrowseController extends Controller
         // simpan key yang tadi diketik ke dalam variable
         // untuk di pass ke view
         // cek link() di result.blade.php
-        $previousSearch = convertSearchKey($request->search);
+        $previousSearch = ubahSearchKey($request->search);
 
 
         return view('result', [
